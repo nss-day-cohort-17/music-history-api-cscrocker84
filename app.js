9053f838542d1e37b646cd62ea1dc7cc34e73163
Charlie Crocker <cscrocker84@gmail.com>
2017-04-19 14:15:46 -0500

    set up my app.js file

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 'use strict'
 
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-let app = express();
 
 // <Include the router index file>
+
+const routes = require('./routes')
+
+let app = express();
+
 // <Setup your routes middleware>
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/v1/', routes)
+
 // <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
+
+app.use(function(req, res, next) {
+    var err = new Error('No Soup for You!');
+    err.status = 404;
+    next(err);
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log('listening on port ${port} in this bodacious env: ${process.env.NODE_ENV}');
+})
+// <Handle any errors that occur in the routing with error handlers defined at the bottom of our
 // middleware stack>
